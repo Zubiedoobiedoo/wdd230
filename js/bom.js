a55d75cb45305d7ee8e45dd85c4cf440bea680be
Zubiedoobiedoo <pou18013@byui.edu>
2022-06-01 13:30:55 -0600

    bom fix

--- js/bom.js
+++ js/bom.js
@@ -1,17 +1,26 @@
-const input = document.querySelector("#chapter");
-const button = document.querySelector("button");
-const list = document.querySelector (".list");
+const inputTag = document.querySelector("#chapter");
+const buttonTag = document.querySelector("button");
+const ulTag = document.querySelector(".list");
 
-button.addEventListener("click", () => {
-    const newElement = document.createElement("li");
-    const deleteBtn = document.createElement("button");
+buttonTag.addEventListener("click", () => {
+  let inputValue = inputTag.value;
+  if (inputValue.trim() === "") {
+  } 
+  else {
+    inputTag.value = "";
+    let liElement = document.createElement("li");
+    let spanElement = document.createElement("span");
+    let buttonElement = document.createElement("button");
+    liElement.appendChild(spanElement);
+    liElement.appendChild(buttonElement);
+    spanElement.textContent = inputValue;
+    buttonElement.textContent = "❌";
 
-    newElement.textContent = input.value;
-    newElement.appendChild(deleteBtn);
-    deleteBtn.textContent = "❌";
-    input.value="";
+    ulTag.appendChild(liElement);
 
-    list.appendChild(newElement);
-    deleteBtn.addEventListener("click", ()=>{list.removeChild(newElement)});
-
+    buttonElement.addEventListener("click", () => {
+      ulTag.removeChild(liElement);
+      inputTag.focus();
+    });
+  }
 });
