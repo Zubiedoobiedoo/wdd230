c1c51e2c635042e00b808bc469b3748a265f8ae3
Zubiedoobiedoo <pou18013@byui.edu>
2022-07-01 17:28:27 -0600

    adfskj;l

--- lesson11/js/directory.js
+++ lesson11/js/directory.js
@@ -1,51 +1,70 @@
-let json = require("json/data.json");
-console.log(json);
-let divElem = document.querySelector(".grid");
-async function getCompanies(){
-const response = await fetch("json/data.json");
-    if (response.ok){
-        const data = await response.json();
-        console.log(data.companies);
-        showcompanies(data.companies);
-    }
-}
-function showcompanies(companies){
-    companies.forEach(company => {
-        let businessCard = document.createElement("section");
-        businessCard.classList.add("business-card");
-        let companyLogo = document.createElement("img");
-        companyLogo.setAttribute("src", company.logos.small);
-        companyLogo.setAttribute("alt", `${company.name}'s logo`);
-        let name = document.createElement("h2");
-        name.textContent = company.name;
-        name.classList.add("company-name");
-        let address = document.createElement("p");
-        address.textContent = company.address;
-        let phone = document.createElement("p");
-        phone.textContent = company.phone;
-        let email = document.createElement("p");
-        email.textContent = company.email;
-        let website = document.createElement("a");
-        website.href = company.website;
-        website.target = "_blank";
-        website.innerHTML = `<p>${company.website}</p>`;
-        businessCard.appendChild(companyLogo);
-        businessCard.appendChild(name);
-        businessCard.appendChild(address);
-        businessCard.appendChild(phone);
-        businessCard.appendChild(email);
-        businessCard.appendChild(website);
-        divElem.appendChild(businessCard);
+const requestURL = "https://zubiedoobiedoo.github.io/wdd230/lesson11/json/data.json";
+const cards = document.querySelector(".cards");
+
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject)
+        const business = jsonObject['companies'];
+        business.forEach(displayBusiness);
     });
-}
-getCompanies();
-const gridBtn = document.querySelector("#grid");
-const listBtn = document.querySelector("#list");
-gridBtn.addEventListener("click", () => {
-    divElem.classList.remove("list");
-    divElem.classList.add("grid");
+
+function displayBusiness(business) {
+    let card = document.createElement("section");
+    let img = document.createElement("img");
+    let name = document.createElement("h1");
+    let pNum = document.createElement("p");
+    let Addr = document.createElement("p");
+    let website = document.createElement("a");
+    let membership = document.createElement("p");
+
+
+    
+    img.setAttribute('src', business.logo);
+    img.setAttribute('alt', `${business.businessName} Logo`);
+    img.setAttribute('loading', 'lazy');
+    img.classList.add("bImg");
+
+    name.textContent = business.businessName;
+    name.classList.add("bName");
+    Addr.textContent = business.address;
+    Addr.classList.add("bAddr");
+    pNum.textContent = business.phone;
+    pNum.classList.add("bNum");
+    website.textContent = business.url;
+    membership.textContent = business.membershipLevel;
+
+    // if(business.website == "N/A") {
+        // business.website = "https://www.mcdonalds.com/us/en-us.html";
+        business.website = "https://kellyo-sm.github.io/wdd230/chamber/404.html";
+    // };
+
+    website.setAttribute('href', `${business.website}`);
+    website.classList.add("bWeb");
+
+    card.appendChild(img);
+    card.appendChild(name);
+    card.appendChild(Addr);
+    card.appendChild(pNum);
+    card.appendChild(website);
+    card.appendChild(membership);
+
+    document.querySelector(".cards").appendChild(card);
+};
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+
+gridbutton.addEventListener("click", () => {
+
+	cards.classList.add("grids");
+	cards.classList.remove("lists");
 });
-listBtn.addEventListener("click", () => {
-    divElem.classList.remove("grid");
-    divElem.classList.add("list");
+
+listbutton.addEventListener("click", () => {
+	cards.classList.add("lists");
+	cards.classList.remove("grids");
 });
